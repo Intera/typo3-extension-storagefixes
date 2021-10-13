26bf0cccd62388d09aa8278710b761800e08755d
Alexander Stehlik <alexander.stehlik@gmail.com>
2021-10-13 19:14:44 +0200

    [TASK] Require TYPO3 9 / PHP 7.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,9 +17,9 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Alexander Stehlik',
     'author_email' => 'astehlik.deleteme@intera.de',
     'author_company' => 'Intera GmbH',
-    'version' => '1.0.0',
+    'version' => '9.0.0-dev',
     'constraints' => [
-        'depends' => ['typo3' => '7.6.0-7.6.99'],
+        'depends' => ['typo3' => '9.5.0-9.5.99'],
         'conflicts' => '',
         'suggests' => [],
     ],
